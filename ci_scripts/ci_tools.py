41511eeee133618300e5755ee169df54b61135f1
dante-signal31 <dante.signal31@gmail.com>
2018-01-08 12:08:11 +0100

    Fixed call to sys.version_info.

--- ci_scripts/ci_tools.py
+++ ci_scripts/ci_tools.py
@@ -4,7 +4,7 @@ import subprocess
 
 
 def run_console_command(command):
-    if sys.version.major < 3:
+    if sys.version_info.major < 3:
         subprocess.call(command, shell=True)
     else:
         subprocess.run(command, shell=True, check=True)
