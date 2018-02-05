899d6f768e40306e5136015d432ea4172ccf97ff
dante-signal31 <dante.signal31@gmail.com>
2018-02-05 20:48:21 +0100

    Trying to externalize ci constant to a common library.

---
+++ ci_scripts/ci_constants.py
@@ -0,0 +1,7 @@
+import os
+
+BRANCH_TO_MERGE_INTO = "test_production"
+BRANCH_TO_MERGE = "travis_integration"
+GITHUB_URL = "https://github.com/"
+GITHUB_REPO = "dante-signal31/vdist.git"
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
