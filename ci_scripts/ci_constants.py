fb15b29221467900de07a971b0f372a7a8d66588
dante-signal31 <dante.signal31@gmail.com>
2018-02-17 17:13:56 +0100

    Branches changed to definitive ones.

--- ci_scripts/ci_constants.py
+++ ci_scripts/ci_constants.py
@@ -1,7 +1,7 @@
 import os
 
-BRANCH_TO_MERGE_INTO = "test_production"
-BRANCH_TO_MERGE = "travis_integration"
+BRANCH_TO_MERGE_INTO = "master"
+BRANCH_TO_MERGE = "staging"
 GITHUB_URL = "https://github.com/"
 GITHUB_REPO = "dante-signal31/vdist.git"
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
